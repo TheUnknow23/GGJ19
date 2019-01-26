dbde39b6b69c90f1964510b043256e372a0ed7f1
TheUnknow23 <32764086+TheUnknow23@users.noreply.github.com>
2019-01-26 01:44:06 +0100

    Backup 1:43 AM

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour
+{
+    //Public variables
+    
+    
+    //Methods
+    public void LoadGame()
+    {
+        StartCoroutine(LoadAsynchronous(SceneManager.GetActiveScene().buildIndex+1));
+    }
+
+    IEnumerator LoadAsynchronous(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUIManager : MonoBehaviour
+{
+    //Public variables
+    public GameObject[] Hearts = new GameObject [3];
+    public PauseMenu PauseMenu;
+
+    //Private variables
+    private List<GameObject> hearts = new List<GameObject>();
+    
+    private void Start()
+    {
+        hearts.Add(Hearts[0]);
+        hearts.Add(Hearts[1]);
+        hearts.Add(Hearts[2]);
+    }
+
+    public void RemoveLife()
+    {
+        hearts.RemoveAt(hearts.Count-1);
+    }
+
+    public void Resume()
+    {
+        PauseMenu.Resume();
+    }
+    
+    public void LoadMenu()
+    {
+        StartCoroutine(LoadAsynchronous(SceneManager.GetActiveScene().buildIndex-1));
+    }
+    
+    IEnumerator LoadAsynchronous(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
